14237170522f6efa5082b6bbe131e1f65a4956b2
Andrew Burchett <andrew.t.w.burchett@gmail.com>
2020-02-18 21:14:45 -0700

    completed exercise 5

--- scripts/store.js
+++ scripts/store.js
@@ -3,7 +3,45 @@ import item from './item.js';
 const items = [];
 const hideCheckedItems = false;
 
+function findById(id) {
+  return store.items.find(item => item.id === id);
+}
+
+function addItem(name) {
+  try {
+    item.validateName(name);
+    this.items.push(item.create(name));
+  }
+  catch (error) {
+    console.log('This is wrong!');
+  }
+}
+
+function findAndToggleChecked(id) {
+  const toggle = this.findById(id);
+  toggle.checked = !toggle.checked;
+}
+
+function findAndUpdateName(id, newName) {
+  try {
+    item.validateName(newName);
+    this.findById(id).name = newName;
+  }
+  catch (error) {
+    console.log(`Cannot update name: ${error.message}`);
+  }
+}
+
+function findAndDelete(id) {
+  this.items = this.items.filter(item => item.id !== id);
+}
+
 export default {
   items, 
-  hideCheckedItems
-}
+  hideCheckedItems,
+  findById,
+  addItem,
+  findAndToggleChecked,
+  findAndUpdateName,
+  findAndDelete
+};
