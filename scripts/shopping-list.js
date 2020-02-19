3be0b9d73b769f34fa5127668afe1dac17cb77a9
Andrew Burchett <andrew.t.w.burchett@gmail.com>
2020-02-19 08:55:59 -0700

    finished exercise 7 and module

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -64,10 +64,6 @@ const handleNewItemSubmit = function () {
   });
 };
 
-// const toggleCheckedForListItem = function (id) {
-//   const foundItem = store.items.find(item => item.id === id);
-//   foundItem.checked = !foundItem.checked;
-// };
 
 const handleItemCheckClicked = function () {
   $('.js-shopping-list').on('click', '.js-item-toggle', event => {
@@ -87,10 +83,7 @@ const getItemIdFromElement = function (item) {
  * Responsible for deleting a list item.
  * @param {string} id 
  */
-// const deleteListItem = function (id) {
-//   const index = store.items.findIndex(item => item.id === id);
-//   store.items.splice(index, 1);
-// };
+
 
 const handleDeleteItemClicked = function () {
   // like in `handleItemCheckClicked`, we use event delegation
@@ -104,17 +97,13 @@ const handleDeleteItemClicked = function () {
   });
 };
 
-// const editListItemName = function (id, itemName) {
-//   const item = store.items.find(item => item.id === id);
-//   item.name = itemName;
-// };
 
 /**
  * Toggles the store.hideCheckedItems property
  */
-const toggleCheckedItemsFilter = function () {
-  store.hideCheckedItems = !store.hideCheckedItems;
-};
+// const toggleCheckedItemsFilter = function () {
+//   store.hideCheckedItems = !store.hideCheckedItems;
+// };
 
 /**
  * Places an event listener on the checkbox
@@ -122,7 +111,7 @@ const toggleCheckedItemsFilter = function () {
  */
 const handleToggleFilterClick = function () {
   $('.js-filter-checked').click(() => {
-    toggleCheckedItemsFilter();
+    store.toggleCheckedFilter();
     render();
   });
 };
--- scripts/store.js
+++ scripts/store.js
@@ -36,6 +36,10 @@ function findAndDelete(id) {
   this.items = this.items.filter(item => item.id !== id);
 }
 
+function toggleCheckedFilter() {
+  this.hideCheckedItems = !this.hideCheckedItems;
+}
+
 export default {
   items, 
   hideCheckedItems,
@@ -43,5 +47,6 @@ export default {
   addItem,
   findAndToggleChecked,
   findAndUpdateName,
-  findAndDelete
+  findAndDelete,
+  toggleCheckedFilter
 };
